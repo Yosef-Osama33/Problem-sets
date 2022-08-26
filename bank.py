1c4e52c8d49ba9fe8d87e90156d96db8a631f0df
Yosef-Osama33 <108608237+Yosef-Osama33@users.noreply.github.com>
2022-08-26 08:06:19 +0200

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,10 +1,15 @@
-x = input("Greeting: ")
-
-new_x = x.lower()
-
-if "hello" in new_x:
+'''
+problem set conditionals
+In a file called bank.py, implement a program that prompts the user for a greeting.
+If the greeting starts with an “h” (but not “hello”), output $20.
+Otherwise, output $100. Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
+'''
+greet = input("Greeting: ")
+if "hello" in greet.lower():
     print("$0")
-elif x[0] == "h":
+
+elif greet.lower().index("h") in [0]:
     print("$20")
+
 else:
     print("$100")
