ea9f15fe9059a364390932e217fc9812515bc66e
Yosef-Osama33 <108608237+Yosef-Osama33@users.noreply.github.com>
2022-08-26 17:58:38 +0200

    Update extension.py

--- extension.py
+++ extension.py
@@ -1,12 +1,37 @@
-x = input("File name: ")
+'''
+problem set conditional
+ a file called extensions.py, implement a program that prompts the user for the name of a file and then outputs that file’s media type 
+ if the file’s name ends, case-insensitively, in any of these suffixes:
 
-x = x.lower()
+.gif
+.jpg
+.jpeg
+.png
+.pdf
+.txt
+.zip
+'''
 
-if ".gif" in x:
+file= input("File name: ")
+
+file= file.lower().strip()
+
+if ".gif" in file:
     print("image/gif")
-elif ".jpg" in x:
-    print("image/jpeg") 
-elif ".jpeg" in x:
-    print("image/jpeg") 
+elif ".jpg" in file:
+    print("image/jpeg")
+elif ".jpeg" in file:
+    print("image/jpeg")
+elif ".png" in file:
+    print("image/png")
+elif ".pdf" in file:
+    print("application/pdf")
+elif ".txt" in file:
+    print("text/plain")
+elif ".zip" in file:
+    print("application/zip")
+elif ".pdf" in file:
+    print("application/pdf")
+
 else:
     print("application/octet-stream")
