e18131b9e96c136e02fa6d1376d3dc4c64ccb459
Yosef-Osama33 <108608237+Yosef-Osama33@users.noreply.github.com>
2022-08-28 20:53:18 +0200

    Update twttr.py

--- twttr.py
+++ twttr.py
@@ -1,10 +1,12 @@
-input = input("Input: ")
-print("Out put: ", end="" )
+'''
+In a file called twttr.py, implement a program that prompts the user for a str of text and then outputs that same text
+    but with all vowels (A, E, I, O, and U) omitted, whether inputted in uppercase or lowercase.
+'''
+x = input("Input: ")
+print("Out put: ", end="")
 
-for letter in input:
+for letter in x:
 
-
-    if not letter in [ "o ", "a" , "e", "i" , "u"]:
-        print(letter, end="")
-        
+    if not letter.lower() in [ "o", "a" , "e", "i" , "u"]:
+        print(letter, end ="")
 
