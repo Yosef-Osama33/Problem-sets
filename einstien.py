2d2e25f2e482c4c8cd5a20a4a98142e503e0ab2b
Yosef-Osama33 <108608237+Yosef-Osama33@users.noreply.github.com>
2022-08-29 21:43:27 +0200

    Update einstien.py

--- einstien.py
+++ einstien.py
@@ -1,3 +1,7 @@
+'''
+In a file called einstein.py, implement a program in Python that prompts the user for mass as an integer (in kilograms)
+and then outputs the equivalent number of Joules as an integer. Assume that the user will input an integer.
+'''
 m = int(input("m: "))
 n = (m *90000000000000000)
 print(f"E: {n}")
