be212ea09531c32f5f08ffecd936f700c265084d
Yosef-Osama33 <108608237+Yosef-Osama33@users.noreply.github.com>
2022-08-29 21:46:12 +0200

    Update depp.py

--- depp.py
+++ depp.py
@@ -1,3 +1,7 @@
+'''
+In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything,
+outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
+'''
 answer = input("What is the Answer to the Great Question of life, the Universe, and Everything? ")
 if answer == ("42") :
     print("Yse")
