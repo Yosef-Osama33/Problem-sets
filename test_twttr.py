19542fdc23e74473b0a0781caf9c8930a3298369
Yosef-Osama33 <108608237+Yosef-Osama33@users.noreply.github.com>
2022-08-29 22:19:37 +0200

    Update test_twttr.py

--- test_twttr.py
+++ test_twttr.py
@@ -1,10 +1,14 @@
+'''
+In a file called twttr.py, reimplement Setting up my twttr from Problem Set 2, restructuring your code per the below,
+wherein shorten expects a str as input and returns that same str but with all vowels (A, E, I, O, and U) omitted,
+whether inputted in uppercase or lowercase.
+'''
 from twttr import shorten
 
 def main():
     test_upper_lower_case()
     test_number()
     test_puncitotation()
-
 
 def test_upper_lower_case():
     assert shorten ("twitter") == "twttr"
@@ -16,11 +20,6 @@ def test_number():
 
 def test_puncitotation():
     assert shorten (".?!,") == ".?!,"
-
-
-
-
-
 
 if __name__ == "__main__":
     main()
