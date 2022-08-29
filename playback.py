5a410b479e610c9cd45c26622b9f162940e9f50f
Yosef-Osama33 <108608237+Yosef-Osama33@users.noreply.github.com>
2022-08-29 21:40:39 +0200

    Update playback.py

--- playback.py
+++ playback.py
@@ -1,2 +1,6 @@
+'''
+In a file called playback.py, implement a program in Python that prompts the user for input and then outputs that same input,
+replacing each space with ... (i.e., three periods).
+'''
 x = input("")
 print(x.replace(" ", "..."))
