10980b3fcadd4f048da8581e3f9adac2d7ccbef3
Yosef-Osama33 <108608237+Yosef-Osama33@users.noreply.github.com>
2022-08-29 21:57:44 +0200

    Update fuel.py

--- fuel.py
+++ fuel.py
@@ -1,3 +1,9 @@
+'''
+In a file called fuel.py, implement a program that prompts the user for a fraction, formatted as X/Y,
+wherein each of X and Y is an integer, and then outputs, as a percentage rounded to the nearest integer,
+how much fuel is in the tank. If, though, 1% or less remains, output E instead to indicate that the tank is
+essentially empty. And if 99% or more remains, output F instead to indicate that the tank is essentially full.
+'''
 while True:
 
     x = input("Fraction: ")
