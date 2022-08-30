2fbf48774370fb2234f4b26dd2135bab983f4309
Yosef-Osama33 <108608237+Yosef-Osama33@users.noreply.github.com>
2022-08-30 18:19:55 +0200

    Update einstien.py

--- einstien.py
+++ einstien.py
@@ -1,4 +1,5 @@
 '''
+problem set variables
 In a file called einstein.py, implement a program in Python that prompts the user for mass as an integer (in kilograms)
 and then outputs the equivalent number of Joules as an integer. Assume that the user will input an integer.
 '''
