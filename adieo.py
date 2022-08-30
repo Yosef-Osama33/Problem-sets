77c57422b69c207301403b3dfebb303a59148cc8
Yosef-Osama33 <108608237+Yosef-Osama33@users.noreply.github.com>
2022-08-30 18:12:22 +0200

    Update adieo.py

--- adieo.py
+++ adieo.py
@@ -1,4 +1,5 @@
 '''
+problem set library
 In a file called adieu.py, implement a program that prompts the user for names, one per line,
 until the user inputs control-d. Assume that the user will input at least one name.
 Then bid adieu to those names, separating two names with one and, three names with two commas and one and,
