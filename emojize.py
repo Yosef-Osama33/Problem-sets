932ae140c6090f09ebb8ee35e595d808d6901470
Yosef-Osama33 <108608237+Yosef-Osama33@users.noreply.github.com>
2022-08-30 18:20:21 +0200

    Update emojize.py

--- emojize.py
+++ emojize.py
@@ -1,4 +1,5 @@
 '''
+problem set library
 In a file called emojize.py, implement a program that prompts the user for a str in English and then
 outputs the “emojized” version of that str, converting any codes (or aliases) therein to their corresponding emoji.
 '''
