b1384b96507f656c974f66c38cacc9e27d86b5ae
Yosef-Osama33 <108608237+Yosef-Osama33@users.noreply.github.com>
2022-09-01 16:35:20 +0200

    Update coke.py

--- coke.py
+++ coke.py
@@ -5,17 +5,18 @@ each time informing the user of the amount due. Once the user has inputted at le
 output how many cents in change the user is owed. Assume that the user will only input integers,
 and ignore any integer that isn’t an accepted denomination.
 '''
-amount =  50
+amount = 50
 while amount > 0:
-    print ("Amount Due" , amount)
+    print(f"Amount Due: {amount}")
 
-    coin = int(input("Insert Coin: "))
+    coin = int(input("Insert coin: "))
 
-    if coin in [10 , 25 , 5]:
-        amount = (amount - coin)
-        print(amount)
- 
+    if coin in [5, 15, 25]:
+        amount = amount - coin
 
+
+owed =  abs(amount)
+print(f"Change Owed: {owed}")
 
 
 
